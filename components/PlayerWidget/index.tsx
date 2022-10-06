ae926e8dac73daae1fbc61f69edfa75c31ed575b
ihajar <hadjero.hamaidi@gmail.com>
2022-10-06 19:52:14 +0100

    Adding SongPlayer

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,17 +1,35 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './styles'
 import { Song } from '../../types'
 import { AntDesign, FontAwesome } from '@expo/vector-icons'
+import { Audio } from 'expo-av'
+
 
 const song = {
     id: '1',
+    uri: 'https://soundcloud.com/taylormusic2019/18-dont-blame-me-live-2018?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
     imageUrl: 'https://s.abcnews.com/images/Entertainment/ABC_taylor_swift_jef_140818_4x3t_992.jpg',
     title: 'Blank Space',
     artist: 'Taylor Swift',
 }
 
 const PlayerWidget = () => {
+
+    const onPlaybackStatusUpdate = (status) => {
+        console.log(status);
+    }
+    const playCurrentSong = async () => {
+        const { sound } = Audio.Sound.createAsync(
+             { uri: song.uri },
+             { shouldPlay: true },
+             onPlaybackStatusUpdate
+        )
+    }
+    useEffect(() => {
+        // play the song
+        playCurrentSong();
+    }, [])
     
     return (
         <View style={styles.container}>
